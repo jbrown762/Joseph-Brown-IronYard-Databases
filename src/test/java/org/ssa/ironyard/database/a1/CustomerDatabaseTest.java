aa55848efc52f46c69f338ef3aa573e70e7ebc5d
Joe Brown <jbrown762@gmail.com>
2016-08-30 16:44:24 -0400

    update

--- src/test/java/org/ssa/ironyard/database/a1/CustomerDatabaseTest.java
+++ src/test/java/org/ssa/ironyard/database/a1/CustomerDatabaseTest.java
@@ -18,35 +18,46 @@ public class CustomerDatabaseTest
     public String URL = "jdbc:mysql://localhost/ssa_bank?user=root&password=root";
 
     @Before
-    public void setupDB() throws SQLException{
+    public void setupDB() throws SQLException
+    {
         MysqlDataSource mysqlDataSource = new MysqlDataSource();
         mysqlDataSource.setUrl(URL);
         db = new CustomerDatabase(mysqlDataSource);
     }
-    
+
     @After
-    public void teardown() throws SQLException{
+    public void teardown() throws SQLException
+    {
         db.connection.close();
     }
-    
+
     @Test
     public void test() throws SQLException
     {
         Customer thurston = db.insert(new Customer("Thur", "Ston"));
-        
+
         assertEquals(new Customer("Thur", "Ston", 35), thurston);
-        
+
         assertTrue(db.delete(new Customer("Joe", "Brown", 28)));
         assertEquals(null, db.read(28));
-        
+
         assertFalse(db.delete(new Customer("Joe", "Brown", 30)));
-        
+
         assertEquals(new Customer("J", "B", 13), db.read(13));
+
+        assertEquals(19, db.read().size());
+        assertEquals(2, db.readLastName("Ston").size());
+        assertEquals(16, db.readFirstName("Joe").size());
+
+        System.out.println(db.read());
+        System.out.println(db.readLastName("Ston"));
+        System.out.println(db.readFirstName("Joe"));
         
-       System.out.println(db.read());
-       System.out.println(db.readLastName("Ston"));
-       System.out.println(db.readFirstName("Joe"));
-       //db.clear();
+        // !!! UNCOMMENT TO CLEAR DATABASE !!!
+        //
+        // db.clear();
+        // assertEquals(0, db.read().size());
+
     }
 
 }
